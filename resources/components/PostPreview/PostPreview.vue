cf798e9ceb51689dba2e1b2a27223c3dde805180
vvmsecured <vvmspace@gmail.com>
2018-06-17 22:40:22 +0500

    Step 1

---
+++ resources/components/PostPreview/PostPreview.vue
@@ -0,0 +1,68 @@
+<template>
+  <nuxt-link :to="id">
+    <article class="post-preview">
+      <div class="post-preview-thumbnail" :style="{backgroundImage: 'url(' + thumbnailImage + ')'}"></div>
+      <div class="post-preview-content">
+        <h2>{{ title }}</h2>
+        <p>{{ excerpt }}</p>
+      </div>
+    </article>
+  </nuxt-link>
+</template>
+
+
+<script>
+  export default {
+    props: {
+      title: {
+        type: String,
+        required: true
+      },
+      excerpt: {
+        type: String,
+        required: true
+      },
+      thumbnailImage: {
+        type: String,
+        required: true
+      },
+      id: {
+        type: String,
+        required: true
+      }
+    }
+  }
+</script>
+
+<style scoped>
+  a {
+    text-decoration: none;
+    color: black;
+  }
+
+  .post-preview {
+    border-radius: 3px;
+    box-shadow: 1px 1px 5px 1px rgba(0, 0, 0, 0.5);
+    width: 90%;
+    margin: auto;
+  }
+
+  .post-preview-thumbnail {
+    background-position: center;
+    background-size: cover;
+    width: 100%;
+    height: 10rem;
+  }
+
+  .post-preview-content {
+    text-align: center;
+    padding: 1rem;
+  }
+
+  @media (min-width: 35rem) {
+    .post-preview {
+      width: 25rem;
+      margin: 1rem;
+    }
+  }
+</style>
--- resources/pages/index.vue
+++ resources/pages/index.vue
@@ -1,15 +1,55 @@
 <template>
-  <section class="container">
-
+  <section id="posts">
+  <PostPreview
+    v-for="post in posts"
+    :key="post.id"
+    :id="post.id"
+    :title="post.title"
+    :thumbnailImage="post.thumbnailUrl"
+    :excerpt="post.previewText"
+  ></PostPreview>
   </section>
 </template>
 
 <script>
-  export default {
 
+  import PostPreview from "@/components/PostPreview/PostPreview";
+
+  export default {
+    components: {PostPreview},
+    data() {
+      return {
+        posts: [
+          {
+            title: "A New Beginning",
+            previewText: "A New Beginning of previewText",
+            thumbnailUrl: "/biggl.jpg",
+            id: 'a-new-beginning'
+          },
+          {
+            title: "A Second Beginning",
+            previewText: "A Second Beginning of previewText",
+            thumbnailUrl: "/biggl.jpg",
+            id: 'a-second-beginning'
+          },
+
+        ]
+      }
+    }
   }
 </script>
 
 <style>
-
+  #posts{
+    padding-top: 2rem;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+    flex-direction: column;
+  }
+  @media (min-width: 35rem) {
+    #posts{
+      flex-direction: row;
+    }
+  }
 </style>
